0db5786ddfc8840a5a239c2b09177f8e5ee3b4c4
artem-p <artempugachev1@gmail.com>
2018-06-09 14:05:14 +0300

    simple dict

---
+++ dictandmaps/SimpleDict.kt
@@ -0,0 +1,35 @@
+package dictandmaps
+
+import java.util.*
+
+
+/**
+ * https://www.hackerrank.com/challenges/30-dictionaries-and-maps/problem
+ * */
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val addressBook = hashMapOf<String, String>()
+
+    val number = scan.nextLine().trim().toInt()
+
+    for (count in 1..number) {
+
+        val inputPhoneAndName = scan.nextLine().split(" ")
+        val name = inputPhoneAndName[0]
+        val phone = inputPhoneAndName[1]
+
+        addressBook[name] = phone
+    }
+
+
+    while (true) {
+        val name = scan.nextLine()
+
+        if (name in addressBook) {
+            println("$name=${addressBook[name]}")
+        } else {
+            println("Not found")
+        }
+    }
+}
