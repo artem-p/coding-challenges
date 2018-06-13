6e04cc06d2145dec65decbfb1d36c58399e79dae
artem-p <artempugachev1@gmail.com>
2018-06-13 17:43:42 +0300

    inheritance

--- src/main/java/classes/Inheritance.java
+++ src/main/java/classes/Inheritance.java
@@ -1,51 +1,73 @@
 package src.main.java.classes;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
+
+class Person {
+    protected String firstName;
+    protected String lastName;
+    protected int idNumber;
+
+    // Constructor
+    Person(String firstName, String lastName, int identification){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.idNumber = identification;
+    }
+
+    // Print person data
+    public void printPerson(){
+        System.out.println(
+                "Name: " + lastName + ", " + firstName
+                        + 	"\nID: " + idNumber);
+    }
+
+}
+
+
+class Student extends Person {
+    private int[] testScores;
+
+    /*
+     *   Class Constructor
+     *
+     *   @param firstName - A string denoting the Person's first name.
+     *   @param lastName - A string denoting the Person's last name.
+     *   @param id - An integer denoting the Person's ID number.
+     *   @param scores - An array of integers denoting the Person's test scores.
+     */
+    public Student(String firstName, String lastName, int id, int[] scores) {
+        super(firstName, lastName, id);
+        this.testScores = scores;
+    }
+
+
+    /*
+     *   Method Name: calculate
+     *   @return A character denoting the grade.
+     */
+    // Write your method here
+    public Character calculate() {
+        double average = Arrays.stream(testScores).average().orElse(Double.NaN);
+
+        if (90 <= average && average <= 100) {
+            return 'O';
+        } else if (80 <= average && average < 90) {
+            return 'E';
+        } else if (70 <= average && average < 80) {
+            return 'A';
+        } else if (55 <= average && average <= 70) {
+            return 'P';
+        } else if (40 <= average && average <= 55) {
+            return 'D';
+        } else {
+            return 'T';
+        }
+    }
+}
+
 public class Inheritance {
-
-    class Person {
-        protected String firstName;
-        protected String lastName;
-        protected int idNumber;
-
-        // Constructor
-        Person(String firstName, String lastName, int identification){
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.idNumber = identification;
-        }
-
-        // Print person data
-        public void printPerson(){
-            System.out.println(
-                    "Name: " + lastName + ", " + firstName
-                            + 	"\nID: " + idNumber);
-        }
-
-    }
-
-
-    class Student extends Person {
-        private int[] testScores;
-
-        /*
-         *   Class Constructor
-         *
-         *   @param firstName - A string denoting the Person's first name.
-         *   @param lastName - A string denoting the Person's last name.
-         *   @param id - An integer denoting the Person's ID number.
-         *   @param scores - An array of integers denoting the Person's test scores.
-         */
-        // Write your constructor here
-
-        /*
-         *   Method Name: calculate
-         *   @return A character denoting the grade.
-         */
-        // Write your method here
-    }
-
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String firstName = scan.next();
