69bc019dddb3a34b2c231bb1b7ee5952206965ee
artem-p <artempugachev1@gmail.com>
2018-06-15 12:50:56 +0300

    scope

--- src/main/java/classes/Scope.java
+++ src/main/java/classes/Scope.java
@@ -3,6 +3,9 @@ package src.main.java.classes;
 
 import java.util.Scanner;
 
+/**
+ * https://www.hackerrank.com/challenges/30-scope/problem
+ * */
 
 class Difference {
     private int[] elements;
@@ -12,8 +15,25 @@ class Difference {
         this.elements = elements;
     }
 
-    public void computeDifference() {
 
+    public void computeDifference() {
+        maximumDifference = Integer.MIN_VALUE;
+
+        for (int outerIndex = 0; outerIndex < elements.length; outerIndex++) {
+            int firstElement = elements[outerIndex];
+
+
+            // compute differences between current element and all elements from the right
+            for (int innerIndex = outerIndex + 1; innerIndex < elements.length; innerIndex++) {
+                int secondElement = elements[innerIndex];
+
+                int difference = Math.abs(firstElement - secondElement);
+
+                if (difference > maximumDifference) {
+                    maximumDifference = difference;
+                }
+            }
+        }
     }
 }
 
